57b0ce96689995b71d433b65d840b73fe863dde3
unknown <viczik16@gmail.com>
2023-03-08 02:18:30 +0100

    updated

--- premier/settings.py
+++ premier/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'premier.urls'
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'premier.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-   # 'default': dj_database_url.parse(os.environ['DATABASE_URL']),
+    'default': dj_database_url.parse(os.environ['DATABASE_URL']),
      
 }
 
@@ -121,6 +122,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'app/static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
